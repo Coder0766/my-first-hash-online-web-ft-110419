e911b2e967fb3bc907247050c445a953e467ca4a
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-16 22:24:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"Whale-bone corsets" => "5"} 
+my_hash = {"Bootcamp" => "Flatiron School"} 
   
 end
 
